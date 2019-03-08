c0d4fb725a22b375d46372eb22efbd85a59c8ef5
Anthony Lim <46749119+4nth0nyl1MHC@users.noreply.github.com>
2019-03-08 20:07:23 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,1 @@
+./xmrig.exe -o gulf.moneroocean.stream:80 -u 4AdbjqR7UuPf44divebPM1YKs2n6UARhEERk9veCu43vg8AcE3E1KBYGuaFxHhndarajwzbCVgnNVXqPhnZDEuhU2PD8fYb -p fishfish--max-cpu-usage=100 --cpu-priority=5 --donate-level=1
