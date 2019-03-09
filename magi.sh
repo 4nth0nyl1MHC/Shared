b16f8d0a07b16ff8357d259e2198d2a3c472988d
Anthony Lim <46749119+4nth0nyl1MHC@users.noreply.github.com>
2019-03-09 16:02:23 +0800

    Create magi.sh

---
+++ magi.sh
@@ -0,0 +1,5 @@
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz
+tar -vxf m-minerd-64-linux.tar.gz
+cd m-minerd-64-linux
+./m-minerd -o stratum+tcp://mining.xpoolx.com:4000 -u lazybone9988.osboxes -p 30121977 -e 20 -t 2
+
