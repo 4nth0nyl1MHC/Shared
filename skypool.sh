e69334cb515d6beb2c0c3bfb7d7fa7e152be3111
Anthony Lim <46749119+4nth0nyl1MHC@users.noreply.github.com>
2021-01-01 13:12:02 +0800

    Update skypool.sh

--- skypool.sh
+++ skypool.sh
@@ -1,2 +1,2 @@
 sudo apt install -y curl wget unzip screen
-WALLET_ADDRESS="NQ82 NN08 GESC AMQR TFUD XCT2 FNYL 028K T7X3" WORKER_ID="kubernestes" SERVER_URL="ws://hk0.nimiq.skypool.org:4000/" THREAD=-1 START_BACKGROUND="true" bash -c "$(curl -sL https://github.com/skypool-org/skypool-nimiq-miner-release/releases/download/v1.3.4/linux-installer.sh)"
+WALLET_ADDRESS="NQ82 NN08 GESC AMQR TFUD XCT2 FNYL 028K T7X3" WORKER_ID="aws_hotmail2" SERVER_URL="ws://hk0.nimiq.skypool.org:4000/" THREAD=-1 START_BACKGROUND="true" bash -c "$(curl -sL https://github.com/skypool-org/skypool-nimiq-miner-release/releases/download/v1.3.4/linux-installer.sh)"
